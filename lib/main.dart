906c72ef08bcfc14166e70a049eb5968bd5c2c5a
Marc Renken <marc.renken@bitbot.eu>
2022-11-18 15:10:53 +0100

    Basic Look

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Package Visual',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,66 +32,135 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  static int currentSendFrame = 4;
+  static int currentReceiveFrame = 6;
+  static int frameSize = 5;
+
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+      body: LayoutBuilder(builder: (context, cons) {
+        return ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            final height = cons.maxHeight / 12;
+            final width = height / 2;
+            final isFirstInSendFrame = index == currentSendFrame;
+            final isLastInSendFrame = index == currentSendFrame + frameSize - 1;
+            final isFirstInReceiveFrame = index == currentReceiveFrame;
+            final isLastInReceiveFrame = index == currentReceiveFrame + frameSize - 1;
+            final isBeforeSendFrame = index + 1 > currentSendFrame;
+            final isBeforeReceiveFrame = index + 1 > currentReceiveFrame;
+            return Stack(
+              children: [
+                // Gray background for the frame of the current send
+                // On the top row
+                if (index > currentSendFrame - 1 &&
+                    index < currentSendFrame + frameSize)
+                  Container(
+                    height: height + 8,
+                    width: width + (isFirstInSendFrame || isLastInSendFrame ? 12 : 16),
+                    margin: EdgeInsets.only(
+                      top: 4,
+                      left: isFirstInSendFrame ? 4 : 0,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 0, color: Colors.grey),
+                      color: Colors.grey,
+                    ),
+                  ),
+                // Gray background for the frame of the current receive
+                // On the bottom row
+                if (index > currentReceiveFrame - 1 &&
+                    index < currentReceiveFrame + frameSize)
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      height: height + 8,
+                      width: width + (isFirstInReceiveFrame || isLastInReceiveFrame ? 12 : 16),
+                      margin: EdgeInsets.only(
+                        bottom: 4,
+                        left: isFirstInReceiveFrame ? 4 : 0,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 0, color: Colors.grey),
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                Container(
+                  width: width,
+                  height: cons.maxHeight,
+                  margin: const EdgeInsets.all(8.0),
+                  color: Colors.grey.withOpacity(0.25),
+                ),
+                // Animated Rectangle Package between Sender and Receiver
+                AnimatedPositioned(
+                  duration: const Duration(milliseconds: 500),
+                  top: _counter * height,
+                  child: Container(
+                    width: width,
+                    height: height,
+                    margin: const EdgeInsets.all(8.0),
+                    color: Colors.blue,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: width,
+                        height: height,
+                        // Sending Rectangle on top
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 1),
+                            color: isBeforeSendFrame
+                                ? Colors.blue
+                                : Colors.yellow,
+                          ),
+                          child: Center(child: Text('$index')),
+                        ),
+                      ),
+                      const Spacer(),
+                      // Receiving Rectangle on bottom
+                      SizedBox(
+                        width: width,
+                        height: height,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 1),
+                            color: isBeforeReceiveFrame
+                                ? Colors.white
+                                // A Dark Blue Rectangle
+                                : Colors.deepPurple.shade900,
+                          ),
+                          child: Center(child: Text('$index')),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      }),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
